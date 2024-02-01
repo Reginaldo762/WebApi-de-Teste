bbcd49e5adcbaf77b79394d37c52e02e2ec88217
Cliente <reginaldogrellmann@gmail.com>
2024-02-01 13:30:51 -0300

    Adicionar arquivos de projeto.

---
+++ Models/apiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+        public DbSet<Users> users { get; set; }
+    }
+}
